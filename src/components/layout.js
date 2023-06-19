015c3a026befc6ff3c5b3c536cec7439cd06fb80
Poylar <doaleks8@gmail.com>
2023-06-19 22:17:52 +0800

    smooth scrolling

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import Header from '@/components/Header';
 import { useGlobalData } from '@/context/GlobalDataContext';
 import { motion } from 'framer-motion';
 import { useEffect } from 'react';
-import SmoothScroll from './SmoothScroll';
+import { ScrollerMotion } from 'scroller-motion';
 
 const Layout = ({ children, globalProps, pageContext }) => {
   const { changeGlobalData } = useGlobalData();
@@ -11,23 +11,23 @@ const Layout = ({ children, globalProps, pageContext }) => {
     changeGlobalData(globalProps);
   });
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{
-        duration: 1,
-      }}
-      className='flex min-h-screen flex-col justify-between text-gray-950'
-    >
-      <SmoothScroll>
+    <ScrollerMotion>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{
+          duration: 1,
+        }}
+        className='text-gray-950'
+      >
         <div className='flex-1 '>
           <Header />
           <div>{children}</div>
           <Footer />
         </div>
-      </SmoothScroll>
-    </motion.div>
+      </motion.div>
+    </ScrollerMotion>
   );
 };
 
