e42e2ca47901c11c6f70c77ac7af601d6ec9e493
Poylar <doaleks8@gmail.com>
2023-06-27 11:40:03 +0800

    test 248

--- src/pages/api/og.jsx
+++ src/pages/api/og.jsx
@@ -42,7 +42,7 @@ export default async function handler() {
             left: '0',
             width: '100%',
             height: '100%',
-            padding: '25px 20px',
+
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'space-between',
