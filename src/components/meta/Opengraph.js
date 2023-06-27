583b87b6f7c6dc1e701cb2160e98e6798da32aa8
Poylar <doaleks8@gmail.com>
2023-06-27 11:12:33 +0800

    fix vercel ur;

--- src/components/meta/Opengraph.js
+++ src/components/meta/Opengraph.js
@@ -5,7 +5,16 @@ const Opengraph = ({ data }) => {
       <meta property='og:type' content='website' />
       <meta property='og:title' content={og_title} />
       <meta property='og:description' content={og_description} />
-      <meta property='og:image' content={`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/og`} />
+      <meta
+        name='og:image'
+        content={
+          // Because OG images must have a absolute URL, we use the
+          // `VERCEL_URL` environment variable to get the deployment’s URL.
+          // More info:
+          // https://vercel.com/docs/concepts/projects/environment-variables
+          `${process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''}/api/og`
+        }
+      />
       <meta property='og:image:width' content='1200' />
       <meta property='og:image:height' content='630' />
       <meta property='og:image:alt' content={og_image_alt} />
