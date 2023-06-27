bf767a1c3d808d9a23c16bb7b6d93a29af115a80
Poylar <doaleks8@gmail.com>
2023-06-27 11:00:09 +0800

    add oh

--- src/components/meta/Opengraph.js
+++ src/components/meta/Opengraph.js
@@ -1,5 +1,3 @@
-import { imageUrlWrapper } from '@/utils/imageUrlWrapper';
-
 const Opengraph = ({ data }) => {
   const { og_title, og_description, og_image, og_image_alt, og_locale, og_locale_alternate, og_site_name } = data;
   return (
@@ -7,11 +5,11 @@ const Opengraph = ({ data }) => {
       <meta property='og:type' content='website' />
       <meta property='og:title' content={og_title} />
       <meta property='og:description' content={og_description} />
-      <meta property='og:image' content={imageUrlWrapper(og_image?.replace(/^\/+/, ''))} />
-      <meta property='og:image:alt' content={og_image_alt} />
+      <meta property='og:image' content={`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/og`} />
       <meta property='og:image:width' content='1200' />
-
       <meta property='og:image:height' content='630' />
+      <meta property='og:image:alt' content={og_image_alt} />
+
       <meta property='og:locale' content={og_locale} />
       <meta property='og:locale:alternate' content={og_locale_alternate} />
       <meta property='og:url' content={og_site_name} />
